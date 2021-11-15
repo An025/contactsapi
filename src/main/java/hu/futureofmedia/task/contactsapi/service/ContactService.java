27590ff6756f5231be8fbce82f38c951ee6c7b56
Ani <jancso.anita@gmail.com>
2021-11-15 05:26:43 +0100

    Check phone validation with libphonenumber

--- src/main/java/hu/futureofmedia/task/contactsapi/service/ContactService.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/service/ContactService.java
@@ -1,5 +1,8 @@
 package hu.futureofmedia.task.contactsapi.service;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
 import hu.futureofmedia.task.contactsapi.entities.*;
 
 import hu.futureofmedia.task.contactsapi.repositories.ContactRepository;
@@ -58,15 +61,36 @@ public class ContactService {
 
     public ResponseEntity<String> addContact(ContactSaveDTO contactSaveDTO, Company company){
         Contact contact = convertContactSaveDTOToContact(contactSaveDTO, company);
+        //Check email validation
         String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
         boolean validEmail = patternMatches(contact.getEmail(),regex);
-
         if(!validEmail){
             return new ResponseEntity<>("Not valid email", HttpStatus.FORBIDDEN);
         }
+        //Check phonenumber validation
+        if (contact.getTelephonenumber() != null){
+            boolean validPhoneNumber = checkPhoneNumber(contact.getTelephonenumber());
+            if(!validPhoneNumber){
+                return new ResponseEntity<>("Not a valid phonenumber", HttpStatus.FORBIDDEN);
+            }
+        }
         contact.setStatus(Status.ACTIVE);
         contactRepository.save(contact);
-        return new ResponseEntity<>("Save contact to database", HttpStatus.OK);
+        return new ResponseEntity<>("Save contact to database", HttpStatus.ACCEPTED);
+    }
+
+    private boolean checkPhoneNumber(String telephonenumber) {
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        try{
+            Phonenumber.PhoneNumber number = phoneNumberUtil.parse(telephonenumber, "HU");
+            String formattedNumber = phoneNumberUtil.format(number, PhoneNumberUtil.PhoneNumberFormat.E164);
+            if(!formattedNumber.equals(telephonenumber)){
+                return false;
+            }
+        } catch (NumberParseException e) {
+            System.err.println("NumberParseException was thrown: " + e.toString());
+        }
+        return true;
     }
 
 
